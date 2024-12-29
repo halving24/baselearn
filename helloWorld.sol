8843ddd66b2500d53710254810a1490b00769904
halving24 <bitget.006em@silomails.com>
2024-12-29 14:49:24 +0100

    modif

--- helloWorld.sol
+++ helloWorld.sol
@@ -5,4 +5,7 @@ contract helloWorld {
     function sayHello() external pure returns (string memory) {
         return "hello World";
     }
+    function Greeter(string memory _name) external pure returns (string memory, string memory) {
+        return ("Hello", _name);
+    }
 }
